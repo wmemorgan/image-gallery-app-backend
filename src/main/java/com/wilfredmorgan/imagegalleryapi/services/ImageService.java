59474f93369ab14bd19b338130870e50a37412ba
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-09-08 19:17:39 +0800

    Define ImageService interface

---
+++ src/main/java/com/wilfredmorgan/imagegalleryapi/services/ImageService.java
@@ -0,0 +1,66 @@
+package com.wilfredmorgan.imagegalleryapi.services;
+
+import com.wilfredmorgan.imagegalleryapi.models.Image;
+
+import java.util.List;
+
+/**
+ * The service that works with the Image model
+ */
+public interface ImageService {
+
+    /**
+     * Returns a list of all images
+     *
+     * @return
+     */
+    List<Image> findAll();
+
+    /**
+     * Returns the image with the given primary key
+     *
+     * @param id
+     * @return
+     */
+    Image findImageById(long id);
+
+    /**
+     * Returns a list of images from a specific user
+     *
+     * @param userid
+     * @return
+     */
+    List<Image> findImagesByUser(long userid);
+
+    /**
+     * Given a complete image object, saves that image object in the database
+     * If a primary key is provided, the record is completely replaced
+     * If no primary key is provided, one is automatically generated and the record is added to the database
+     *
+     * @param image The image object to be saved
+     * @return The saved image object including any automatically generated fields
+     */
+    Image save(Image image);
+
+    /**
+     * Updates the provided fields in the image record referenced by the primary key
+     *
+     * @param image
+     * @param id
+     * @return
+     */
+    Image update(Image image, long id);
+
+    /**
+     * Deletes the role record based off the provided primary key
+     *
+     * @param id The primary key (long) of the image to be deleted
+     */
+    void delete(long id);
+
+    /**
+     * Deletes all records and their associated records from the database
+     */
+    void deleteAll();
+
+}
